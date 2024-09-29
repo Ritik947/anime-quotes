ece41223bbfe9930d553480df625c7ba0e860d4d
Ritik947 <gourritikkailash@gmail.com>
2024-09-29 17:47:06 +0530

    working??

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
-(async ()=>{
+(async () => {
   let btn = document.getElementById("btn-submit");
-  let wrap = document.getElementById("wrapper");
   btn.addEventListener("click", () => {
     btn.classList.toggle("invisible");
     showQuote();
   });
-})();
+}) ();
 
-function showQuote() {
-  const url = 'https://animechan.io/api/v1/quotes/random';
+async function showQuote() {
+  try {
+    const url = 'https://animechan.io/api/v1/quotes/random';
+    const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`);
+    if (response.status != 200) {
+      console.log("Encountered an error \n Code " + response.status);
+      return;
+    }
+    const data = await response.json();
+    const quote_data = JSON.parse(data.contents).data;
+    let wrap = document.getElementById("wrapper");
+    let box = document.createElement("div");
+    let source = document.createElement("div");
+    source.id = "source";
+    box.id = "quote";
+    wrap.appendChild(box);
+    box.innerText = quote_data.content;
+    wrap.appendChild(source);
+    source.innerHTML = quote_data.character.name + "<br/>(" + quote_data.anime.name + ")";
+  } catch (error) {
+    console.error(error) // from creation or business logic
+  }
   // const url = 'https://anime-quotes2.p.rapidapi.com/api/random';
   // const host = config["applications"]["anime_quotes"]["API_KEYS"][0]["host_name"]
   // const api_key = config["applications"]["anime_quotes"]["API_KEYS"][0]["api_key"]
@@ -20,28 +39,10 @@ function showQuote() {
   //     'x-rapidapi-host': host
   //   }
   // };
-  fetch(url).then(function (res) {
-    if (res.status != 200) {
-      console.log("Encountered an error \n Code " + res.status);
-      return;
-    }
-    res.json().then(function (data) {
-      let quote = data;
-      console.log(quote);
-      let box = document.createElement("div");
-      let source = document.createElement("div");
-      source.id = "source";
-      box.id = "quote";
-      wrap.appendChild(box);
-      box.innerText = quote.content;
-      wrap.appendChild(source);
-      source.innerHTML = quote.character.name + "<br/>(" + quote.anime.name + ")";
-    });
-  });
 }
 
-async function get_config(request) {
-  const response = await fetch(request);
-  const body = await response.json();
-  return body;
-}
+// async function get_config(request) {
+//   const response = await fetch(request);
+//   const body = await response.json();
+//   return body;
+// }
